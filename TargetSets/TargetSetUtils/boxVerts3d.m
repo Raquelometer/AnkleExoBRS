9fb6685522510eba2601ada104ad8a1ed755be26
Raquelometer <daphnaraz@gmail.com>
2024-04-01 12:25:30 -0400

    Updated target set code

--- TargetSets/TargetSetUtils/boxVerts3d.m
+++ TargetSets/TargetSetUtils/boxVerts3d.m
@@ -1,5 +1,8 @@
 function [verts] = boxVerts3d(axes, eqPt)
 
+% Compute vertices of the minimum circumscribed box
+% Shift if eq is not at origin.
+
 vert1 = [-axes(1) -axes(2) -axes(3)] + eqPt';
 vert2 = [-axes(1) -axes(2) axes(3)] + eqPt';
 vert3 = [-axes(1) axes(2) -axes(3)] + eqPt';
